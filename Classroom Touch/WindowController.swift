6c146dabd119056833bc78227241270498b22291
Bas van der Hoek <bas@hoek.com>
2019-11-27 13:40:31 +0100

    scrubber

--- Classroom Touch/WindowController.swift
+++ Classroom Touch/WindowController.swift
@@ -1,16 +1,33 @@
 import Cocoa
 
-class WindowController: NSWindowController {
-  
+@available(OSX 10.12.2, *)
+class WindowController: NSWindowController, NSScrubberDataSource {
   @IBOutlet weak var tslider: NSSlider!
-
+  @IBOutlet weak var tscrubber: NSScrubber!
+  let sid = NSUserInterfaceItemIdentifier(rawValue: "TextScrubberItemIdentifier")
   override func windowDidLoad() {
     super.windowDidLoad()
     tslider.action = #selector(sliderChanged)
+    tscrubber.dataSource = self
+    tscrubber.register(NSScrubberTextItemView.self, forItemIdentifier: sid)
   }
   
   @objc func sliderChanged() {
     print(tslider.doubleValue)
   }
+}
+
+private typealias ScrubberDataSource = WindowController
+@available(OSX 10.12.2, *)
+extension ScrubberDataSource {
+  func numberOfItems(for scrubber: NSScrubber) -> Int {
+    100
+  }
   
+  func scrubber(_ scrubber: NSScrubber, viewForItemAt index: Int) -> NSScrubberItemView {
+    let itemView = scrubber.makeItem(withIdentifier: sid, owner: self) as! NSScrubberTextItemView
+      
+    itemView.title = String(index)
+    return itemView
+  }
 }

6feb14a8444d7eb8d753e8e74c2c8f3363ca6fc7
Bas van der Hoek <bas@hoek.com>
2019-11-29 16:19:05 +0100

    keep multipliers in array with ranges

--- Classroom TouchTests/Classroom_TouchTests.swift
+++ Classroom TouchTests/Classroom_TouchTests.swift
@@ -1,51 +1,60 @@
-//
-//  Classroom_TouchTests.swift
-//  Classroom TouchTests
-//
-//  Created by Bas van der Hoek on 29/11/2019.
-//  Copyright © 2019 Zilverline. All rights reserved.
-//
-
 import XCTest
 
 class Classroom_TouchTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  func secondsr(_ percent: Int, _ ranges: [(Range<Int>, Int)]) -> Int {
+    if let t = ranges.first {
+      let delta = min(t.0.upperBound - t.0.lowerBound, percent * t.1)
+      return delta + secondsr(percent - delta / t.1, Array(ranges.dropFirst()))
     }
+    return 0
+  }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  func testRecursive() {
+    let ranges = [
+      (0..<(1 * 60), 5),
+      ((1 * 60)..<(3 * 60), 10),
+      ((3 * 60)..<(10 * 60), 30),
+      ((10 * 60)..<(30 * 60), 60),
+      ((30 * 60)..<(180 * 60), 60 * 5),
+      ((180 * 60)..<(500 * 60), 60 * 10),
+    ]
+    XCTAssertEqual(secondsr(6, ranges), 30)
+    XCTAssertEqual(secondsr(13, ranges), 70)
+    XCTAssertEqual(secondsr(25, ranges), 210)
+    XCTAssertEqual(secondsr(39, ranges), 11 * 60)
+    XCTAssertEqual(secondsr(59, ranges), 35 * 60)
+    XCTAssertEqual(secondsr(89, ranges), 190 * 60)
+    XCTAssertEqual(secondsr(100, ranges), 5 * 60 * 60)
+  }
 
-    func secondser(_ percent: Int) -> Int {
-      let ranges = [
-               (0..<(1*60),   5),
-          ((1*60)..<(3*60),   10),
-          ((3*60)..<(10*60),  30),
-         ((10*60)..<(30*60),  60),
-         ((30*60)..<(180*60), 60 * 5),
-        ((180*60)..<(500*60), 60 * 10),
-      ]
-      var seconds = 0
-      var remainder = percent
-      ranges.forEach { (t) in
-        let delta = min(t.0.upperBound - t.0.lowerBound, remainder * t.1)
-        remainder -= delta / t.1
-        seconds += delta
-      }
-      return seconds
+  func seconds(_ percent: Int) -> Int {
+    let ranges = [
+      (0..<(1 * 60), 5),
+      ((1 * 60)..<(3 * 60), 10),
+      ((3 * 60)..<(10 * 60), 30),
+      ((10 * 60)..<(30 * 60), 60),
+      ((30 * 60)..<(180 * 60), 60 * 5),
+      ((180 * 60)..<(500 * 60), 60 * 10),
+    ]
+    var seconds = 0
+    var remainder = percent
+    ranges.forEach { (t) in
+      let delta = min(t.0.upperBound - t.0.lowerBound, remainder * t.1)
+      remainder -= delta / t.1
+      seconds += delta
     }
-  
-    func testExample() {
-      XCTAssertEqual(secondser(6), 30)
-      XCTAssertEqual(secondser(13), 70)
-      XCTAssertEqual(secondser(25), 210)
-      XCTAssertEqual(secondser(39), 11 * 60)
-      XCTAssertEqual(secondser(59), 35 * 60)
-      XCTAssertEqual(secondser(89), 190 * 60)
-      XCTAssertEqual(secondser(100), 5 * 60 * 60)
-    }
+    return seconds
+  }
 
+  func testLooped() {
+    XCTAssertEqual(seconds(6), 30)
+    XCTAssertEqual(seconds(13), 70)
+    XCTAssertEqual(seconds(25), 210)
+    XCTAssertEqual(seconds(39), 11 * 60)
+    XCTAssertEqual(seconds(59), 35 * 60)
+    XCTAssertEqual(seconds(89), 190 * 60)
+    XCTAssertEqual(seconds(100), 5 * 60 * 60)
+  }
 
 }
